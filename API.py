458aa2ffb26b3342619a94b4ea7d02bb2a02a510
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-01-19 18:30:20 -0600

    made a skeleton API weather caller

--- API.py
+++ API.py
@@ -1,8 +1,14 @@
-import request
+import requests
 
-API_KEY = "API key here!"
+API_KEY = ""
 
-URL = f"Here"
-reponse = request.get(URL)
+URL = "http://api.openweathermap.org/data/2.5/forecast?"
 
-print(response.status_code)
+CALL = f"{URL}id={ID}&appid{API_KEY}" 
+
+response = requests.get(CALL)
+
+if response.status_code == 200:
+    print()
+else:
+    print(f"Error: {response.status_code}")
