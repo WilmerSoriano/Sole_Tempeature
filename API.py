358656630b1543d2b3c84c732bc87651982655a4
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-01-19 20:31:30 -0600

    An envrionement was downloaded to keep the API safe while also allowing python to read the API

--- API.py
+++ API.py
@@ -1,13 +1,17 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+# Load variables from .env into environment
+load_dotenv()
 
 CityName = 'Dallas'
 
-API_KEY = os.getenv("OPEN_API_KEY") # Keeping API Key a Secret
+API_KEY = os.getenv('OPEN_API_KEY') # Keeping API Key a Secret
 
-URL = "https://api.openweathermap.org/data/2.5/weather?"
+URL = "https://api.openweathermap.org/data/2.5/weather"
 
-CALL = f"{URL}q={CityName}&appid{API_KEY}&units= imperial" 
+CALL = f"{URL}?q={CityName}&appid={API_KEY}&units=imperial"
 
 response = requests.get(CALL)
 
