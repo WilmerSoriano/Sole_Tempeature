1f506794cfb53a27d819586b3b5bf00d3ce8c1b3
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-01-19 19:49:01 -0600

    Added API in secret and city location. In debugging process. Error 401

--- API.py
+++ API.py
@@ -1,14 +1,18 @@
+import os
 import requests
 
-API_KEY = ""
+CityName = 'Dallas'
 
-URL = "http://api.openweathermap.org/data/2.5/forecast?"
+API_KEY = os.getenv("OPEN_API_KEY") # Keeping API Key a Secret
 
-CALL = f"{URL}id={ID}&appid{API_KEY}" 
+URL = "https://api.openweathermap.org/data/2.5/weather?"
+
+CALL = f"{URL}q={CityName}&appid{API_KEY}&units= imperial" 
 
 response = requests.get(CALL)
 
 if response.status_code == 200:
-    print()
+    data = response.json()
+    print(data)
 else:
     print(f"Error: {response.status_code}")
