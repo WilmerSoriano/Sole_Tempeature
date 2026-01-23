46fbb1a1909ffd5f8442e91aca43197e8265ca51
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-01-22 22:18:21 -0600

    Made a function to transfer data over to app UI

--- API.py
+++ API.py
@@ -2,26 +2,33 @@ import os
 import requests
 from dotenv import load_dotenv
 
-# Load variables from .env into environment
-load_dotenv()
+def fetch_data():
+    # Load variables from .env into environment
+    load_dotenv()
 
-# Location: Dallas, Texas, US
-CityName = "Dallas"
+    # Location: Dallas, Texas, US
+    CityName = "Dallas"
 
-# Keeping API Key a Secret
-API_KEY = os.getenv("OPEN_API_KEY")
+    # Keeping API Key a Secret
+    API_KEY = os.getenv("OPEN_API_KEY")
 
-URL = "https://api.openweathermap.org/data/2.5/weather"
+    URL = "https://api.openweathermap.org/data/2.5/weather"
 
-CALL = f"{URL}?q={CityName}&appid={API_KEY}&units=imperial"
+    CALL = f"{URL}?q={CityName}&appid={API_KEY}&units=imperial"
 
-response = requests.get(CALL)
+    response = requests.get(CALL)
 
-if response.status_code == 200:
-    data = response.json()
-    print(f"Dallas {data["main"]["temp"]}F")
-    print("Feels Like:", data["main"]["feels_like"])
-    print("H:", data["main"]["temp_max"])
-    print("L:", data["main"]["temp_min"])
-else:
-    print("Error:", response.status_code)
+    if response.status_code == 200:
+        data = response.json()
+
+        temp = float(data["main"]["temp"])
+        h_temp = float(data["main"]["temp_max"]) 
+        l_temp =  float(data["main"]["temp_min"])
+        
+        return temp, h_temp, l_temp
+    else:
+        print("Error:", response.status_code)
+
+if __name__ == "__main__":
+    temp, h_temp, l_temp = fetch_data()
+    print(temp, h_temp, l_temp)
